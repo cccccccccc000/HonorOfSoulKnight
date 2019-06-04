627365c5106d6c6d59f483ac86ee18090cd581aa
chenhouyu <573749373@qq.com>
2019-06-04 14:02:00 +0800

    To delete the unnecessary files

--- src/MENU.h
+++ src/MENU.h
@@ -4,7 +4,7 @@
 
 #include "cocos2d.h"
 
-class HelloWorld : public cocos2d::Scene
+class MENU : public cocos2d::Scene
 {
 public:
     static cocos2d::Scene* createScene();
@@ -15,7 +15,7 @@ public:
     void menuCloseCallback(cocos2d::Ref* pSender);
     
     // implement the "static create()" method manually
-    CREATE_FUNC(HelloWorld);
+    CREATE_FUNC(MENU);
 };
 
 #endif // __HELLOWORLD_SCENE_H__
