38b1eb48f7f679df3f9f49ac5a0cb87a116540b8
chenhouyu <573749373@qq.com>
2019-06-04 16:08:01 +0800

    遵守命名约定

--- src/AppDelegate.cpp
+++ src/AppDelegate.cpp
@@ -95,7 +95,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = MENU::createScene();
+    auto scene = Menu::createScene();
 
     // run
     director->runWithScene(scene);
